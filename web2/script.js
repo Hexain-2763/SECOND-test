79119a4588a2eff48b3d2f7b300efdbf32b3dc9f
Raymond Chen <thebluehexagon0@gmail.com>
2026-06-24 21:04:11 -0500

    SecondWeb

---
+++ web2/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('actionButton');
+
+button.addEventListener('click', () => {
+  const message = 'Hello from web2! This site is fully independent of web1.';
+  alert(message);
+});
